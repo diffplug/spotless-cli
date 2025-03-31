14cbfd47be7022124b8c1e1be51d6d931450829c
Simon Gamma <github@survive.ch>
2025-03-31 19:58:29 +0200

    chore: add more test variants for prettier

---
+++ app/src/test/java/com/diffplug/spotless/cli/steps/PrettierJavaProcessTest.java
@@ -0,0 +1,6 @@
+package com.diffplug.spotless.cli.steps;
+
+import com.diffplug.spotless.tag.CliProcessNpmTest;
+
+@CliProcessNpmTest
+public class PrettierJavaProcessTest extends PrettierTest{}
---
+++ app/src/test/java/com/diffplug/spotless/cli/steps/PrettierNativeProcessTest.java
@@ -0,0 +1,6 @@
+package com.diffplug.spotless.cli.steps;
+
+import com.diffplug.spotless.tag.CliNativeNpmTest;
+
+@CliNativeNpmTest
+public class PrettierNativeProcessTest extends PrettierTest{}
